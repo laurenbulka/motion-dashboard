b236c24974c14f60611291d6a21b520ce802daf4
laurenbulka <lab439@drexel.edu>
2020-11-17 23:41:51 -0500

    updated files

--- dev/js/logo.js
+++ dev/js/logo.js
@@ -9,7 +9,8 @@ export function drawAnimation(){
 
     //drawTL.from("#rebel-logo",{duration:3, drawSVG: "0%", ease:"none"});
 
-    drawTL.fromTo("#rebel-logo",{drawSVG:"0% 10%"},{duration:3.5, drawSVG: "90% 100%"})
+    drawTL.fromTo("#rebel-logo",{drawSVG:"0% 10%"},{duration:2, drawSVG: "90% 100%", ease:"none"})
           .to("#rebel-logo",{duration:2, drawSVG: "100%"});
+          drawTL.to("#rebel-logo", {duration:1, scale: .4, x:120, y:-100})
 
     return drawTL}
